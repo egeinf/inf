3c4485828f5c63218bc13568c2ab688f576dec5b
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-05-03 10:30:02 +0300

    123

--- ege/19.py
+++ ege/19.py
@@ -6,7 +6,7 @@ def f(x, p):
 for s in range(1, 42):
     if f(s, 1):
         print(s, end=' ')
-print()
+
 print('Задача 20:')
 def f(x, p):
     if x >= 42 or p > 4: return p == 4
@@ -17,7 +17,7 @@ def f(x, p):
 for s in range(1, 42):
     if f(s, 1):
         print(s, end=' ')
-print()
+
 print('Задача 21:')
 def f(x, p):
     if x >= 42 or p > 5: return p == 3 or p == 5
@@ -28,7 +28,9 @@ def f(x, p):
 for s in range(1, 42):
     if f(s, 1):
         print(s, end=' ')
-# 2 КУЧИ
+
+
+########################### 2 КУЧИ
 print('Задача 19:')
 def f(x, y, p):
     if x + y >= 77 or p > 3: return p == 3
