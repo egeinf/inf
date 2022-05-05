d5af47b960997a3ba0d9625fbd2336759c005f04
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-05-05 14:54:34 +0300

    8 upd

--- ege/8.py
+++ ege/8.py
@@ -48,3 +48,17 @@ for a1 in a:
                     if a1 == 'У' and 'АА' not in x:
                       print(count, x)
                       break
+            
+a = 'ИЕАУОЯ'
+k = 0
+for a1 in a:
+    for a2 in a:
+        for a3 in a:
+            for a4 in a:
+                for a5 in a:
+                    b = a1+a2+a3+a4+a5
+                    if a5 == 'Е': # оканчивается на "Е"
+                        if a1 != a3 and a2 != a4 and a3 != a5: # не совпадают буквы идущие через одну
+                          if 'А' in b:
+                            k += 1
+                            print(k, b)
