c687cfb9ff49cf84c410f47b3fd9734a711a6a6f
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-05-22 20:48:30 +0000

    20

---
+++ 20.py
@@ -0,0 +1,9 @@
+def f(x,y,p):
+    if x+y >= 223 or p > 4: return p == 4
+    if p % 2 != 0:
+        return f(x+1, y, p+1) or f(x, y+1, p+1) or f(x*2,y,p+1) or f(x,y*2,p+1)
+    else:
+        return f(x + 1, y, p + 1) and f(x, y + 1, p + 1) and f(x * 2, y, p + 1) and f(x, y * 2, p + 1)
+for s in range(1, 205):
+    if f(17,s,1):
+        print(s, end=' ')
